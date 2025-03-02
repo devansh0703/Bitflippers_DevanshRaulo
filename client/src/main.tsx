007497f0090d21cda32e9bc3e0dadb3b10c8d63b
b3tar00t <74288865+b3tar00t@users.noreply.github.com>
2025-03-02 10:20:32 +0530

    Delete client directory

--- client/src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-// TomTom CSS is imported in index.html
-import App from "./App";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
